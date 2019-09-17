4e5f714c734da05e66d3e33b3ddd1904cdd020da
Hanns Niedermark <hanns@niedermark.de>
2019-09-17 16:58:04 +0100

    added counter

--- CBD_tool.py
+++ CBD_tool.py
@@ -4,3 +4,9 @@ with open("C:\\Users\\hanns\\Desktop\\TwintCBDWorkfile.csv", 'r', encoding = "ut
     lines = data.readlines()
 for i in range(len(lines)):
     print(lines[i])
+
+def Tweet_Counter():
+    counter = 0
+    for tweet in lines:
+        counter += 1
+    print(counter - 2)
