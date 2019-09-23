1f047b503002cffdf6e10954ce312a20e711a2eb
Hanns Niedermark <hanns@niedermark.de>
2019-09-23 15:44:27 +0100

    diverse changes: trying to export to csv

--- CBD_tool.py
+++ CBD_tool.py
@@ -1,12 +1,16 @@
 import pandas as pd
 from collections import Counter
 
-
 # importing the CSV
+from pandas import DataFrame
+
 df = pd.read_csv("TwintCBDWorkfile.csv")
+final_df: DataFrame = pd.DataFrame(columns=['X', 'Y', 'Z'])
+top_5_liked_df: DataFrame = pd.DataFrame(columns=['X', 'Y', 'Z'])
+topfiver_df: DataFrame = pd.DataFrame(columns=['X', 'Y', 'Z'])
+most_active_accounts_df: DataFrame = pd.DataFrame(columns=['X', 'Y', 'Z'])
 
-
-randomtext = "$DCGD, already heading to big boom for 25Bil merger CBD banking and dis"
+# randomtext = "$DCGD, already heading to big boom for 25Bil merger CBD banking and dis"
 
 def clean_up():
     # drops all the unnecessary columns
@@ -16,25 +20,25 @@ def clean_up():
 
 def tweet_counter():
     # returns the number of tweets in the document
-    print("There are " + str(df.shape[0]) + " tweets in this document.")
+    return "There are " + str(df.shape[0]) + " tweets in this document."
 
 
 def top_5_liked():
     # prints the 5 tweets with the highest like count (username, tweet and likes count)
     ndf = df.sort_values("likes_count", ascending=False).head()
-    print(ndf[["username", "tweet", "likes_count"]])
+    top_5_liked_df = ndf[["username", "tweet", "likes_count"]]
 
 
 def most_active_accounts():
     # returns the 5 accounts with the most tweets including the hashtag/key word
     print("The following accounts have tweeted the most about the topic:")
-    print(df.username.value_counts().head())
+    most_active_accounts_df = df.username.value_counts().head()
 
 
 def top_5_retweeted():
     # prints the 5 tweets with the highest retweet count (username, tweet and likes count)
     nrdf = df.sort_values("retweets_count", ascending=False).head()
-    print(nrdf[["username", "tweet", "retweets_count"]])
+    topfiver_df = nrdf[["username", "tweet", "retweets_count"]]
 
 
 def special_character_cleaning(str_to_clean):
@@ -50,7 +54,7 @@ def special_character_cleaning(str_to_clean):
     return clean_str
 
 
-def keyword_counter():
+"""def keyword_counter():
     keyword_count = {}
     all_words = []
 
@@ -60,10 +64,13 @@ def keyword_counter():
     for i in df.tweet:
         clean_tweets_list.append(special_character_cleaning(i))
     clean_tweets_str = clean_tweets_str.join(clean_tweets_list)
+"""
 
 
-
-
+def export_to_csv():
+    with pd.ExcelWriter('output.xlsx') as writer:
+        df.to_excel(writer, sheet_name='Tweets')
+        final_df.to_excel(writer, sheet_name="Analysis")
 
 
 def run_all():
@@ -74,4 +81,11 @@ def run_all():
     most_active_accounts()
 
 
-keyword_counter()
+def describe():
+    describe_df = df.describe(include="all")
+
+run_all()
+final_df.append(topfiver_df)
+# export_to_csv()
+
+print(final_df)
